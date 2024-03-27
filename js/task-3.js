626d96fc58c0624ee7a6d015aeabe56318b19e75
Dmytro1573 <124176235+Dmytro1573@users.noreply.github.com>
2024-03-27 10:23:52 +0100

    goit-js-hw-07

--- js/task-3.js
+++ js/task-3.js
@@ -4,8 +4,8 @@ const outName = document.querySelector("#name-output");
 userName.addEventListener("input", handleInput);
 function handleInput(event) {
   if (event.currentTarget.value.trim() === "") {
-    outName.textContext = "Anonymous";
+    outName.textContent = "Anonymous";
   } else {
-    outName.textContext = event.currentTarget.value.trim();
+    outName.textContent = event.currentTarget.value.trim();
   }
 }
